c6a4a175e97c52c4d8f9d53c26c4ce6991dd6383
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-28 14:08:58 +0200

    Check if isset a property in the getter trait

--- lib/Getter.php
+++ lib/Getter.php
@@ -2,6 +2,10 @@
 
 class Getter {
 
+	public function __isset(string $name): bool {
+		return isset($this->$name);
+	}
+
 	public function __get(string $name): mixed {
 		return $this->$name ?? null;
 	}
