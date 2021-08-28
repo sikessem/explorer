98e5a5ba6ee95399e94b84aa62eb65cac163b4fa
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-28 17:03:31 +0200

    Improve the request protocol

--- lib/Protocol.php
+++ lib/Protocol.php
@@ -4,19 +4,14 @@ class Protocol {
 
 	use Getter;
   
-  protected string $type;
+  protected string $scheme;
 
   protected string $version;
 
 	public function __construct(string $name) {
-    $this->setName($name);
-	}
-
-  public function setName(string $name): self {
     if(preg_match('/^([A-Z]+)\/([1-9]+(?:\.[0-9]+)?)$/', $name, $matches)) {
-      $this->type = $matches[1];
+      $this->scheme = $matches[1];
       $this->version = $matches[2];
-      return $this;
     }
-  }
+	}
 }
