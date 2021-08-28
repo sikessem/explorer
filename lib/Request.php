005e9a18a4c7972e61ebc61ce99e589749df519c
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-08-28 17:07:27 +0200

    Improve the request class

--- lib/Request.php
+++ lib/Request.php
@@ -4,34 +4,5 @@ class Request {
 
 	use Getter;
 
-	protected Method $method;
-
-	protected URI $uri;
-
-	protected Headers $headers;
-
-	protected Protocol $protocol;
-
-	public function __construct(string $protocol, string $verb, string $uri, array $headers = [], array $data = []) {
-		$this->setMethod($verb, $data);
-		$this->setURI($uri);
-		$this->setHeaders($headers);
-		$this->setProtocol($protocol);
-	}
-
-	public function setMethod(string $verb, array $data = []): Method {
-		return $this->method = new Method($verb, $data);
-	}
-
-	public function setURI(string $uri): URI {
-		return $this->uri = new URI($uri);
-	}
-
-	public function setHeaders(array $headers = []): Headers {
-		return $this->headers = new Headers($headers);
-	}
-
-	public function setProtocol(string $protocol): Protocol {
-		return $this->protocol = new Protocol($protocol);
-	}
+	public function __construct(protected Method $method, protected URI $uri, protected Protocol $protocol, protected Headers $headers) {}
 }
